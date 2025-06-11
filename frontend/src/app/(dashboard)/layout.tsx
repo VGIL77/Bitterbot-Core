fab1526a9db6739fe7abcfa916c7687d5a632ead
VGIL77 <64641464+VGIL77@users.noreply.github.com>
2025-06-11 14:38:11 -0400

    UI fix

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { SidebarLeftNew } from '@/components/sidebar/sidebar-left-new';
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 // import { PricingAlert } from "@/components/billing/pricing-alert"
 import { MaintenanceAlert } from '@/components/maintenance-alert';
 import { useAccounts } from '@/hooks/use-accounts';
